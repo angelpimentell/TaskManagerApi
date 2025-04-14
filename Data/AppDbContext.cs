5e80db82f45bf41b72bde4125f29291fbc2108fe
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-04-14 16:14:12 -0400

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TaskManagerApi.Models.Tasks;
+
+namespace TaskManagerApi.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Models.Tasks.Task<string>> Tasks { get; set; }
+    }
+}
---
+++ Models/Tasks/Task.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerApi.Models.Tasks
+{
+
+    public class Task<T>
+
+    {
+        [Key]
+        public int Id { get; set; } 
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public required string Status { get; set; } 
+        public T? AdditionalData { get; set; } 
+    }
+
+}
