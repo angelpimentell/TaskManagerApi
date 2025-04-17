d6081f7bdf0abbc6b8d43deb5232c649873b4ab4
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-04-17 11:40:48 -0400

    Add swagger

--- Program.cs
+++ Program.cs
@@ -12,12 +12,16 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddOpenApiDocument();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // app.MapOpenApi();
+    app.UseOpenApi();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
