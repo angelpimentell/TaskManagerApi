1cf0ee41bd56b8904413a374c98e52cede6c621f
Angel Pimentel <angelpimentelcontact@gmail.com>
2025-05-16 10:02:07 -0400

    Add task factory

---
+++ Factories/TaskFactory.cs
@@ -0,0 +1,26 @@
+﻿using TaskManagerApi.Creators;
+
+namespace TaskManagerApi.Factories
+{
+    public class TaskFactory
+    {
+        public const int HIGH_PRIORITY = 3;
+        public const int MEDIUM_PRIORITY = 2;
+        public const int LOW_PRIORITY = 1;
+
+        public static TaskCreator CreateInstance(int type)
+        {
+            switch (type)
+            {
+                case HIGH_PRIORITY:
+                    return new HighPriorityTaskCreator();
+                case MEDIUM_PRIORITY:
+                    return new MediumPriorityTaskCreator();
+                case LOW_PRIORITY:
+                    return new LowPriorityTaskCreator();
+                default:
+                    return new LowPriorityTaskCreator();
+            }
+        }
+    }
+}
