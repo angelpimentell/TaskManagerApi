971c70b460daea1a4fb03e317ee2682487ada9e2
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-06-09 18:02:14 -0400

    Add SignalR

---
+++ NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TaskManagerApi
+{
+    public class NotificationHub : Hub
+    {
+        public async Task Send(string Message)
+        {
+            await Clients.All.SendAsync(Message);
+        }
+    }
+}
